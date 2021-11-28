a3042bea6001bd34f66721726a898f672dd8a201
acstermann <acstermann@gmail.com>
2021-11-28 22:33:43 +0100

    original commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import logo from "./images/dictionary.jpg";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
