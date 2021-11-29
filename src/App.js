38e2288d2796d1b8e7fb3df48ce6aaddcfd9abf0
acstermann <acstermann@gmail.com>
2021-11-29 07:04:21 +0100

    include search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import logo from "./images/dictionary.jpg";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by A.Stermann</footer>
+      </div>
     </div>
   );
 }
